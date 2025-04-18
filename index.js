19c9e3ff9209113127daab3a3d05dbef0ea0450e
Mark Wells <61658816+john-wells-ai1@users.noreply.github.com>
2025-04-18 09:55:15 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-analytics.js";
- import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-admin.js";
+ import { admin } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-admin.js";
 
 // TODO: Add SDKs for Firebase products that you want to use
  // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,7 +21,7 @@
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const db = getFirestore(app);
+const db = admin.firestore();
 
 // test Firebase db interface
 console.log("calling notesCollection");
