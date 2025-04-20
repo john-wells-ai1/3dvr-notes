95895bdba5b01c362fcf1e84ed91413428dc6cf3
John Wells <john.wells3333@gmail.com>
2025-04-19 20:39:33 -0700

    Add server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getFirestore, collection, doc, getDoc, getDocs, addDoc, updateDoc, deleteDoc } from "firebase/firestore";
+
+import pkg from 'express';
+const express = pkg;
+
+import path from 'path';
+
+const app = express();
+const port = 3000; // You can use any port number
+
+// Serve static files from the same directory as server.js
+app.use(express.static('.'));
+
+// Route to serve the HTML file
+app.get('/', (req, res) => {
+  res.sendFile(path.join('main.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
